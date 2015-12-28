17f32a96231b28d08b58abbefb39d1b6fc2a008e
SeekArt <studio8@sohu.com>
2015-12-28 19:39:27 +0800

    update

--- config.php
+++ config.php
@@ -10,18 +10,18 @@ return array(
 'host' => '127.0.0.1',
 'port' => '3306',
 'dbname' => 'ibos',
-'username' => 'root',
-'password' => 'root',
+'username' => 'admin',
+'password' => '-pwd-',
 'tableprefix' => 'ibos_',
 'charset' => 'utf8'
 ),
 // -------------------------  CONFIG SECURITY  -------------------------- //
 'security' => array(
-'authkey' => '2eb8748IiGzMu27D',
+'authkey' => '2eb8749IiGzMu27D',
 ),
 // --------------------------  CONFIG COOKIE  --------------------------- //
 'cookie' => array(
-'cookiepre' => 'MdSf_',
+'cookiepre' => 'MeSf_',
 'cookiedomain' => '',
 'cookiepath' => '/',
 )
